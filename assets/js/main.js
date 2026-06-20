d2be5b2d98bf51adf7a29a224ceac019617e1850
Lil.wide.codes <lilwidez@gmail.com>
2026-06-20 22:17:37 +0300

    data stucturing day two

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,12 @@ window.addEventListener("scroll", () => {
   const percent = (current / height) * 100;
   progress.style.width = percent + "%";
 });
+
+/* The cta button
+const ctaButton = document.querySelector(".cta-button");
+
+//event listener for the cta button
+ctaButton.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default behavior of the button
+  alert("BUTTON WAS CLICKED!..."); // Display an alert message
+}); */
